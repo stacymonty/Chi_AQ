359f77ec05d9b3e2c7cfcc4b8c21796c9a826577
Anastasia (Stacy) Montgomery <40803590+stacymonty@users.noreply.github.com>
2021-11-30 13:27:19 -0600

    Update make_gif.py

--- make_gif.py
+++ make_gif.py
@@ -7,20 +7,6 @@ import glob
 filestartswith='NO2'
 dir='~/'
 
-# pull grid stuff
-#Get number of files in directory
-onlyfiles = next(os.walk(dir))[2]
-onlyfiles = [x for x in onlyfiles if x.startswith(filestartswith)]
-onlyfiles=sorted(onlyfiles)# so th
-
-fstart=[filestartswith+dates[i] for i in range(len(dates))]
-fnames=[]
-
-for f in onlyfiles: 
-	for fs in fstart: 
-		if f.startswith(fs): fnames.append(f)# Copied from http://superfluoussextant.com/making-gifs-with-python.html tytyty
-
-
 #
 gif_name = 'NO2'
 fps = 10
